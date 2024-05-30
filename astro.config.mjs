48dcb8e89c897d423aa5d6b63e9387b13a035954
15 <15@soiree.horse>
2024-05-30 04:50:26 -0700

    Fix lenis and swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), icon(), swup({ theme: false })]
+  integrations: [tailwind(), sitemap(), icon(), swup({ theme: 'slide' })]
 });
