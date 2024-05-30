1e429b5099cd31eac66e5e9fc0957a55711914d4
15 <15@soiree.horse>
2024-05-30 06:22:51 -0700

    About us

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import swup from '@swup/astro';
+// import swup from "@swup/astro";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), icon(), swup({ theme: 'slide' })]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    icon(),
+    // swup({ theme: "slide", globalInstance: true }),
+  ],
 });
