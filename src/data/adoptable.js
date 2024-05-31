3e1accc63d662b137ae94e5bc4d2e68624c184b3
15 <15@soiree.horse>
2024-05-31 02:44:37 -0700

    Sponsorable

---
+++ src/data/adoptable.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "Ace",
+    url: "https://givebutter.com/MYIlgD",
+    rescueDate: "May 1, 2023",
+    breed: "Rocky Mountain",
+    changeName: true,
+    isMale: true,
+    age: 11,
+    dateOfBirth: 2013,
+    cost: 231,
+    description: "Friendly and lovable like a big puppy-dog, Ace used to be a child’s horse until he contracted EPM, which went untreated for too long. That's when he came to us!"
+  },
+  {
+    name: "Bert",
+    url: "https://givebutter.com/dHrL2i",
+    rescueDate: "October 23, 2023",
+    breed: "Quarter Horse",
+    changeName: true,
+    isMale: true,
+    age: 31,
+    dateOfBirth: 1993,
+    cost: 230,
+    description: "Real laid back and sweet guy, just wants to hang out. Bert came from an Elderly owner who could no longer take care of him but wanted to ensure he could also live out his retirement in a kind of safe place."
+  }
+];
