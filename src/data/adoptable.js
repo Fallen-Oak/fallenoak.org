e91ee404d7fe86ca78d046ac4f52723f164c7388
15 <15@soiree.horse>
2024-05-31 12:10:23 -0700

    Adoptable horses

--- src/data/adoptable.js
+++ src/data/adoptable.js
@@ -1,26 +1,28 @@
 export default [
   {
-    name: "Ace",
+    name: "April",
     url: "https://givebutter.com/MYIlgD",
-    rescueDate: "May 1, 2023",
-    breed: "Rocky Mountain",
-    changeName: true,
-    isMale: true,
-    age: 11,
-    dateOfBirth: 2013,
-    cost: 231,
-    description: "Friendly and lovable like a big puppy-dog, Ace used to be a child's horse until he contracted EPM, which went untreated for too long. That's when he came to us!"
+    rescueDate: "2023",
+    breed: "Arab Cross",
+    rideable: true,
+    isMale: false,
+    age: 20,
+    dateOfBirth: 2004,
+    cost: 500,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
-    name: "Bert",
+    name: "Derby",
     url: "https://givebutter.com/dHrL2i",
-    rescueDate: "October 23, 2023",
-    breed: "Quarter Horse",
-    changeName: true,
+    rescueDate: "March 15, 2023",
+    breed: "Appendix",
+    rideable: true,
     isMale: true,
-    age: 31,
-    dateOfBirth: 1993,
-    cost: 230,
-    description: "Real laid back and sweet guy, just wants to hang out. Bert came from an Elderly owner who could no longer take care of him but wanted to ensure he could also live out his retirement in a kind of safe place."
-  }
+    age: 18,
+    dateOfBirth: 2006,
+    cost: "N/A",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
 ];
