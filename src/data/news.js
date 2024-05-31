8c2e09567abcc6da0e4f4042632fbe837a14ac17
15 <15@soiree.horse>
2024-05-31 05:56:12 -0700

    News

---
+++ src/data/news.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    author: "Kim Sweeney",
+    title: "Semi's Adoption",
+    content: `Semi has been adopted!
+
+    Semi came to us last summer and was first adopted out in September but, sadly, he was having some riding issues and didn’t work out with his adopter. At some point in Semi’s life he had some trauma that causes him to become terrified and shake while being ridden in certain situations. As a result, he came back to the rescue on March 7th.
+    Despite his issues with riding, he is a lovely horse with excellent ground manners. So we decided that it was time for Semi to just be a horse and hang out with friends as a companion horse. Months later, the wonderful people at Rainbows Edge Equine Transition Center reached out to us about a lady who was looking for a companion horse.
+    Kim swiftly reached out and invited her and her husband to come visit the rescue and as soon as the couple met Semi they were enthralled with him! Semi now lives in Gainesville Florida on a beautiful farm with his new best friend, Phoenix.
+    
+    We are so thankful for Gloria and her husband for providing Semi with a loving home!`,
+    img: 'Semi_atnewhome_1.jpg',
+    date: "May 31, 2024",
+    id: "semis-adoption"
+  }
+]
