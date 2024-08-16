30ed96a849eae724184ba1714279d36c88415760
15 <146901496+15@users.noreply.github.com>
2024-08-15 22:14:11 -0700

    Lots of fixes

--- src/data/news.js
+++ src/data/news.js
@@ -46,16 +46,16 @@ export default [
   //   author: "Kim Sweeney",
   //   title: "Monthly Donor & Sponsor Perks",
   //   content: `
-  //   To show love for our supporters, we’re excited to present monthly donor and sponsor rewards! 
+  //   To show love for our supporters, we’re excited to present monthly donor and sponsor rewards!
 
   //   Perks will be available to all those who are sponsoring a horse or making a regular contribution to the rescue, with different tiers for all monthly donors ranging from $1 all the way up to $1000 per month. Included in these perks are things like a subscription to the monthly Fallen Oak newsletter, having your name and a bio about you or your business on the site, signed photos of the horses and a whole host of other perks!
-    
+
   //   By sponsoring a horse, they effectively become yours but stay at the rescue to be fed, looked after and cared for. It’s a way to have your very own horse, without having to own the land needed to keep them or being burdened with the responsibility of feeding them and cleaning out the stalls (unless you’d like to come and help with that).
-    
+
   //   Your sponsorship money will be used to cover the horse’s monthly financial needs including food, medication and hoof trims, and you’ll be able to have an input on any of the major decisions regarding your horse’s care. Sponsors also receive regular updates on their special steed, with photos, a small lock of the mane or tail and the option to have a description & photo of you and your horse featured on the website. You can also come out to the rescue and spend some time with the horse and brush them, and for some horses you’ll even have the option to change their name.
-    
+
   //   If you’re interested in sponsoring a horse or making a monthly contribution to the rescue, be sure to check out the sponsor and donor sections of the site!
-    
+
   //   https://fallenoak.org/sponsor/
   //   https://fallenoak.org/donor/
   //   `,
