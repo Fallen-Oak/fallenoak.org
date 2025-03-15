c7550973bf75d945fc62ba354f955526b3795ad5
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-15 01:50:59 -0400

    Fixed comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     define: {
       'import.meta.env.PONY': JSON.stringify(process.env.PONY)
     }
-  }
+  },
   redirects: {
     '/mareday': {
       status: 302,
