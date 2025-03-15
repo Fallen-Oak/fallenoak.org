4672844b9352a54957cf295ed14f57008dfd114e
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-15 03:10:18 -0400

    Debug to Display Pony

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 // import swup from "@swup/astro";
+import icon from "astro-icon";
 
 console.log("PONY:", process.env.PONY);
-
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
