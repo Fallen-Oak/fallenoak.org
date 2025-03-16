1ce754d9760c08b9d1598259d10a8b1b15a9eca8
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-15 22:31:07 -0400

    Added Dinner with Horses announcement

--- src/data/news.js
+++ src/data/news.js
@@ -78,4 +78,19 @@ export default [
     id: "mare-day-2025",
     pony: true,
   },
+  {
+    author: "Brad Colangelo",
+    title: "Dinner with Horses Signups are Open!",
+    content: `We appreciate all the love and support that we've gotten from the community. We’d like to say thank you! So, we’re pleased to announce that Mare Day will be happening again this year. It’s our way of giving back!
+	
+    The event be held at the rescue on September 9. It is a day full of fun activities like horseback rides, grooming horses, spending time with yearlings, farm chores, and more!
+	
+    You can find the registration form below. Tickets are only $20!
+	
+    https://www.zeffy.com/ticketing/mare-day--2025`,
+    img: "Dinner-2024.jpg",
+    date: "March 15, 2025",
+    id: "dinner-with-horses-2025",
+    pony: true,
+  },
 ];
