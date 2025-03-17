823364b842972dd97e0c762f0938f87757ddb00f
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-17 14:24:44 -0400

    Added 2025 newsposts from Facebook

--- src/data/news/news-2025.js
+++ src/data/news/news-2025.js
@@ -1,5 +1,63 @@
 export default [
   {
+    author: "Kim Sweeney",
+    title: "Goodbye, Dakota",
+    content: `It’s been a rough few days. I try to remind myself where there is pain, there is joy just around the corner.
+    
+    Sadly, our sweet Dakota crossed the rainbow bridge on Tuesday. Dakota was a 24-year-old Rocky Mountain horse who came into the rescue last year as a Sanctuary horse, as he had DSLD. The past month he was starting to show signs of pain, and his back left pastern was very close to the ground. Dr. Knowles and I agreed it was better to let him go on a good day before the ligament got worse. It’s never easy making these decisions, but I will always do what’s best for the horses.
+    
+    Run free, sweet boy. Tell Montana I miss her.`,
+    img: "goodbye-dakota.jpg",
+    date: "January 13, 2025",
+    id: "goodbye-dakota",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Joyous Adoptions",
+    content: `Here is my joy from last week. Does everyone remember Honey?
+    
+    She was adopted twice and both times returned emaciated. Also Dusty, he came in in the Spring he was an ex-reining horse. Well guess what? They were both adopted by a wonderful person. I was contacted by Sue a couple weeks ago, looking for two older companion, sanctuary horses. It is not very often that I get requests for this. Everybody wants the younger, kid safe trail horses. I put a lot of thought into who I thought would be a perfect fit.
+    
+    I had said previously that Honey would never leave, and I was adamant in that decision until I met Sue. I have absolutely no doubt that Honey and Dusty will live out the rest of their lives at Sue’s farm. Sue is a wonderful person and has a beautiful set-up. Honey and Dusty deserve one-on-one attention and to have their person. So, last week amongst the sorrow of having to let a couple horses go, I got to experience the joy of knowing I was able to rehabilitate both of these horses and help them find their person. Congrats, Sue on your adoption of these wonderful horses.
+    
+    On days like this, I love what I do.`,
+    img: "joyous-adoptions.jpg",
+    date: "January 14, 2025",
+    id: "joyous-adoptions",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Flash's Adoption",
+    content: `I have the pleasure of announcing an awesome adoption.
+
+    While I was in Ohio over the holidays, I was asked to take a horse into the rescue and find him a home. Talk about a divine appointment while I was driving back! I received a call from Becky. She had adopted from us 6 years ago and had recently lost her horse Rascal. She was looking for another. Flash is a 22 year old, very well trained TN Walker. He belonged to an autistic man for years, who sadly had to go to a group home after his grandparents passed away and their farm was sold.
+    
+    His family needed to make sure Flash was okay for the rest of days. That is why they chose a rescue, and I’m honored they chose mine. Flash was adopted by Becky and will live out the rest of his days at her adorable little farm. I am so happy for this new adventure for Becky and Flash, what a great combo. Brian is content knowing his buddy of 10 years is safe with a lovely lady.
+
+    Congrats to all!`,
+    img: "flashs-adoption.jpg",
+    date: "January 15, 2025",
+    id: "flashs-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Goodbye, Major",
+    content: `When I had originally posted about Dakota, I was considering posting about Major crossing the rainbow bridge at the same time. However, I felt that they each deserved their own post.
+
+    Major was 30+, according to our equine dentist, and an ex-reining horse. Major came in last June with his buddy Dusty, who was adopted two weeks ago. Major was missing nine teeth, had issues with sweet itch, was severely underweight, and had DSLD. Over the past seven months, Major gained over 250 pounds and his sweet itch cleared up.
+    
+    However, about a month ago he was sitting down a lot. When our awesome Ferrier Camp came out to trim him the last time, we could not even trim his back feet. I knew it was time. I had Dr. Knowles examine him and he agreed, so we let him go the same day as Dakota. Losing one horse is hard, but two on one day is unbearable. But I know they are pain free and I did right by them. I will always listen to the horse and do what’s best, no matter how my heart hurts.
+
+    Run free, sweet boy.`,
+    img: "goodbye-major.jpg",
+    date: "January 18, 2025",
+    id: "goodbye-major",
+    pony: false,
+  },
+  {
     author: "Brad Colangelo",
     title: "Mare Day 2025",
     content: `We appreciate all the love and support that we've gotten from the community. We’d like to say thank you! So, we’re pleased to announce that Mare Day will be happening again this year. It’s our way of giving back!
@@ -13,6 +71,19 @@ export default [
     date: "February 8, 2025",
     id: "mare-day",
     pony: true,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Welcome, Alena",
+    content: `I would like to introduce you to Fallen Oak's newest and oldest horse. Welcome, Alena!
+
+    She is a 35 year old level 3 dressage Trakehner. Alena’s previous owner has been unable to provide for her for the last five years, so her wonderful neighbor and her husband stepped up and took over her care. Unfortunately, they are selling their farm and their personal horses, so they needed to find a permanent home for Alena. This is such a sweet girl. She’s in great shape, she does have Cushing's, but is clipped and on medication.
+
+    I am happy to provide this sweet girl a place to live out her days, welcome to the retirement/special needs pasture, Alena!`,
+    img: "welcome-alena.jpg",
+    date: "February 24, 2025",
+    id: "welcome-alena",
+    pony: false,
   },
   {
     author: "Brad Colangelo",
@@ -51,5 +122,22 @@ export default [
     date: "March 15, 2025",
     id: "dinner-with-horses",
     pony: true,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Look Who's Back",
+    content: `I am pleased to announce Magnum is back!
+
+    This 17’2 plus Oldenburg out of Art Deco is one of my favorite rescues of all time. Talk about personality! This guy will be 21 in April and has earned sanctuary for the rest of his days. Magnum came from Ohio almost three years ago after a serious suspensory injury ended his eventing career.
+
+    After lots of rehab, he was deemed sound for light riding and was adopted within days. Magnum's prior adopter returned him after two members of his family became seriously ill, and all of his attention needed to be focused on helping to care for them.
+
+    The rescue will always take back a horse that has been adopted if life circumstances change. It is part of our contract. I am thrilled Magnum is home once again and will remain at my personal farm for the rest of his days. He'll enjoy riding from time to time by my daughter Trinity, who is thrilled he’s back.
+
+    Welcome home Mags, a truly gentle giant.`,
+    img: "look-whos-back.jpg",
+    date: "March 16, 2025",
+    id: "look-whos-back",
+    pony: false,
   },
 ];
