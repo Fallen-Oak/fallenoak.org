3ff201bc56d9a6ef406aff12039412f9339787c9
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-17 20:13:25 -0400

    Added 2024 newsposts from Facebook

--- src/data/news/news-2024.js
+++ src/data/news/news-2024.js
@@ -43,6 +43,214 @@ export default [
     img: "merch-store.jpg",
     date: "July 12, 2024",
     id: "merch-store",
-    pony: true,
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Goodbye, Hunter",
+    content: `I received some sad news. Our sweet Hunter passed away.
+
+    Hunter was the first horse I have ever been able to adopt out that had a serious health issue. Hunter's cancer spread and he began losing a ton of weight, so the decision to let him go was made. Thanks so much to Georgia for loving him in his last year of life and taking such good care of him.
+
+    Run free, sweet boy.`,
+    img: "goodbye-hunter.jpg",
+    date: "August 16, 2024 ",
+    id: "goodbye-hunter",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Honey Update",
+    content: `I was so excited to come back into town and see the difference in our sweet girl.
+
+    Bridgette is doing an incredible job rehabbing Honey. I was so excited to see the difference in just 17 days. I am so thankful for our emergency rehab foster mom, Bridgette, and all she is doing for Honey. Great job!
+
+    I sure do love you sweet girl, you will again be made whole.`,
+    img: "honey-update.jpg",
+    date: "August 17, 2024 ",
+    id: "honey-update",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "A Horse with an Attitude",
+    content: `Hi, ya’ll! I wanted to introduce you to one of our newest residents.
+
+    Give a warm welcome to Shear Attitude, AKA Atty. This lovely OTTB mare has had a lot of babies and several of them have done very well. Atty raced 12 times and then became a broodmare. Her prior owner could no longer afford to take care of her. Atty is a very sweet girl! She’s a bit underweight, but we will fix that. She will live out her days here at Fallen Oak as a sanctuary horse.
+
+    Welcome to the farm, sweet girl.`,
+    img: "a-horse-with-an-attitude.jpg",
+    date: "August 18, 2024 ",
+    id: "a-horse-with-an-attitude",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Welcome Back, Cody",
+    content: `Beautiful people, I would like to introduce Cody.
+
+    Several years ago, Cody was adopted and was used at a horse camp. The kids loved him, however he developed moon blindness and has lost vision in his right eye. I gladly welcomed this 22 year old palomino Rocky Mountain horse back into the rescue. He has been granted sanctuary and will live here at the farm the rest of the days.
+    
+    Cody is such a sweet boy! I’m happy to have him back.`,
+    img: "welcome-back-cody.jpg",
+    date: "August 20, 2024 ",
+    id: "welcome-back-cody",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Two Horses Available for Adoption!",
+    content: `I’m gonna give you a little sneak peek of two incredibly awesome horses that I have available for adoption.
+
+    One is a BLM mustang from Wyoming. He is an 11 year old red roan gelding. The other is a mustang quarter horse cross 15 year old buckskin gelding. Both of these horses have been given excellent care. These guys were owned by two different families that I know extremely well, but life happens. If you are interested, reach out to me. These two will go fast. 2Bits is the bay and Skye is the buckskin. Both are trail horses, current on everything, and Skye has been ridden extensively by kids.
+
+    Stay tuned!
+
+    Update: Both horses have been adopted!`,
+    img: "two-horses-available-for-adoption.jpg",
+    date: "August 23, 2024",
+    id: "two-horses-available-for-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "April's Adoption",
+    content: `I have great news - our sweet girl April was adopted by her foster mom.
+
+    Patty has had April for more than a year and has rehabbed, loved on, and ridden her, and she has flourished. I am so thankful! This is the perfect home and place for April. Thanks Patty for opening your home to an older special needs horse and loving her the way you do. I appreciate you!
+
+    What a great life April will continue to have.`,
+    img: "aprils-adoption.jpg",
+    date: "August 26, 2024 ",
+    id: "aprils-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Skye's Adoption",
+    content: `Skye is officially Colton’s new best friend.
+
+    I am so excited to announce he is thriving with his new family. There was a ton of interest in Skye, and I received and approved several applications. Colton was chosen, as his family has adopted two prior horses and were top of the waiting list for several months.
+
+    Congrats, buddy! I'm so happy for both of you!`,
+    img: "skyes-adoption.jpg",
+    date: "September 8, 2024",
+    id: "skyes-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "2Bits's Adoption",
+    content: `I wanted to let everyone know that 2Bits the Mustang was adopted.
+
+    There was a great deal of people interested in this sweet boy. After careful consideration, he was placed at a wonderful farm to be a lesson horse for kids. It was 2Bits's prior owner's wish that he would become a lesson horse and it came to fruition. I am so thankful for Brittany and her team!
+
+    I can’t wait to see how this guy flourishes. Congrats!`,
+    img: "2bitss-adoption.jpg",
+    date: "September 9, 2024",
+    id: "2bitss-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Honey Completely Rehabbed",
+    content: `Days like today remind me of why I love what I do, and that there is always hope.
+
+    If you recall, Honey came to us on July 7. I have posted her progress on Facebook over the past couple months. This beautiful mare is now completely rehabbed and came back to the farm on Sunday! I am so excited to show this transformation. This was all done by feeding her Triple Crown Senior Maxi Glow Tifton and alfalfa hay twice a day. I am in awe of how she picked this weight up so quickly and I am saddened that she ever got into the shape that she was to begin with.
+
+    So today is a great day, a day that I celebrate rehabilitation - a reminder to me to keep doing what I know I’m called to do. Welcome home, Honey! You will spend the rest of your days in your own retirement pasture with two other sanctuary horses, which hopefully will be a very long time, since you are only 22.
+
+    Words cannot express my gratitude to my foster, Bridgette. I am eternally grateful, as I know Honey is. Also, it takes a village.`,
+    img: "honey-completely-rehabbed.jpg",
+    date: "September 17, 2024",
+    id: "honey-completely-rehabbed",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Name This New Horse",
+    content: `We have a couple new horses in the rescue, and I’m gonna be doing something a little bit different for them. Fallen Oak came together with three other rescues to assist on 15 horses needing to be placed, due to an owner surrender situation. I have received two of those horses. I was going to give everyone an opportunity to get a chance to name the new horses. They are both mares.
+
+    The first girl is an 11-year-old Pasofino. She has a halter on, but will not allow us to get very close. She is so pretty. This sweet girl came in with only a Coggins and is currently in quarantine in my round pen. She will need to have her teeth done, her feet done, be power packed, and receive all of her vaccines. She will need lots of Triple Crown Senior, alfalfa, maxi glow and soaked cubes to put her weight back on. This beauty is only 11 years old. Once she is gentle and completely rehabbed, she will be available for adoption.
+
+    I thought it would be a good idea to allow people to give input on her name, as we have not chosen one for her yet. For every $10 that you donate, you get a chance in the hat to choose her name. If you donate $50, that’s five chances. $100, that’s 10. There is a spot on Zeffy where you can list the horse's name selection. If you would like to PayPal or Venmo, you can also list it that way. If you want to send a check, that’s good also. I’m going to keep this running for 10 days and choose her name on the 30th. I’m excited to see what you guys come up with.
+
+    Also, if anyone is interested in her, please reach out to me and I can put you on the list. Thank you so much for all that you do! Let’s get this sweet girl a really good name.
+
+    You can donate here:
+    https://donate.fallenoak.org/`,
+    img: "name-this-new-horse.jpg",
+    date: "September 20, 2024",
+    id: "name-this-new-horse",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Goodbye, Lucy",
+    content: `Today is a very sad day.
+
+    I came out to feed early this morning, and our sweet Lucy was gone. She was 30 years old. I had the pleasure of loving this sweet girl for almost two years. At 7:30 PM she was fine and was gone within 12 hours. Her buddy Cowboy sure is missing her. I'm putting one of my older mares who recently went blind in with him tonight. Lucy came so far from a bag of bones to looking fabulous, I guess it was just her time.
+
+    Run free, sweet girl. I’ll take care of Derby for you. She was his mama. Tell Earl I sure do miss him.`,
+    img: "goodbye-lucy.jpg",
+    date: "September 24, 2024",
+    id: "goodbye-lucy",
+    pony: false,
+  },
+  {
+    author: "Brad Colangelo",
+    title: "Mare Fair and Mare Day",
+    content: `Fallen Oak was once again honored as the chosen charity for this year’s Mare Fair, taking place from September 27 to September 29. Mare Fair is an My Little Pony convention open to all adult fans. Fallen Oak was also the con’s charity at its inaugural event last year. Kim and Trinity attended and had a few panels about the rescue. The generous attendees raised over $100k for Fallen Oak!
+
+    To say thank you to the community, an event called Mare Day was held at the rescue the Tuesday after the con wrapped up. Activities included learning to draw horses, farm chores, an overview on horse care, horse rides, and special supper called Dinner with Horses.
+
+    If any of this sounds enticing, we hope to see you at the next Mare Day!`,
+    img: "mare-fair-and-mare-day.jpg",
+    date: "October 2, 2024",
+    id: "mare-fair-and-mare-day",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Goodbye, Montana",
+    content: `It's a very sad night tonight at the rescue.
+
+    Our oldest horse here, sweet Montana, went to heaven. I was blessed to have this sweet girl for almost three years. Montana came in January 2022 and was a bag of bones. She was discarded when she could not get pregnant again. It was my pleasure to rehab and love this sweet girl for three of her 33 years. Until we meet again, my sweet girl. Thanks to Dr. Knowles for coming out tonight and helping me make the decision to let her go.
+
+    Run free, you will be so missed.`,
+    img: "goodbye-montana.jpg",
+    date: "November 13, 2024",
+    id: "goodbye-montana",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Happy Thanksgiving!",
+    content: `I'm feeling incredibly thankful today.
+
+    As I cleaned Jewel and Cowboy's paddock, I stopped to think of all the horses in their 30s in the rescue. Cowboy is our oldest now, almost 33 with only 17 teeth. It takes a lot of time and money to take care of the special needs sanctuary horses. They range in age as young as 12 and as old as 32. While cleaning the paddock, I was reminded as I looked at my ponies how blessed I was again this year by Mare Fair. I know we are all more thankful during the week of Thanksgiving.
+    
+    But my friends, I have been incredibly thankful and blessed so much these past 15 months, all because a group of people wanted to help my rescue. The horses and I will forever be grateful for the day that two strangers showed up, who I now call friends, and along with an amazing group of people who generously blessed all the horses of Fallen Oak. We forever thank you️!
+
+    The 25 horses that make up the rescue wish all of you a wonderful holiday. We are so thankful for our incredible three foster homes, our weekly volunteers, Julianne - who rents the rescue 32 acres of her beautiful farm, Kymberly - who feeds the rescue horses every morning and when I’m out of town, Kevin - my awesome farmhand - who is here at least once a week, all of my MLP friends who donate so generously along with all my other donors, all the folks that have adopted horses from the rescue these past four and a half years, and my daughter Trinity - who does all the daily chores with me at both farms. I am so thankful this is my life, for I know it is my calling, and it is something that will continue because of ALL of you.
+
+    Be blessed today and everyday.`,
+    img: "happy-thanksgiving.jpg",
+    date: "November 28, 2024",
+    id: "happy-thanksgiving",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Giving Tuesday",
+    content: `Today is Giving Tuesday. What does that mean to you?
+
+    To me, it is an opportunity for me to donate to causes that need help or support for a variety of reasons. I am a giver, always have been. When I received the proceeds from my fundraiser this year, I donated a in portion of it to three other nonprofits. I believe it’s important to support and help others. The rescue currently has 25 horses, all are sanctuary, except for one who is currently in training. The average cost to feed and maintain one of our sanctuary residents is approximately $475 a month. Some of the older horses are as high as $600 with medications and supplements. The rescue is currently spending about $8,000 a month to properly take care of these horses. If you find it in your heart to donate, please do. You can send a check or pay online at https://donate.fallenoak.org. We also have PayPal. All donations are tax deductible. 
+
+    The horses and I thank you, be blessed.`,
+    img: "giving-tuesday.jpg",
+    date: "December 3, 2024",
+    id: "giving-tuesday",
+    pony: false,
   },
 ];
