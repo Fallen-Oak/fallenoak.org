af12243d134c0e128a76872f41830fe92769811d
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-18 17:58:11 -0400

    Added basics for the videos page

---
+++ src/data/videos.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    title: "Horse Rescue: The Good, the Bad, and the Blessing",
+    description: `This panel was held at Mare Fair 2023.
+
+    Kim and her daughter share how the rescue started, their mission, how many horses they have saved to date, the cost of rehabilitating a horse, and have lots of before and after photos of the horses they've helped.
+
+    They share stories of four very special horses.
+
+    Finally, Kim talks about the different problems that come up when you're rehabilitating a horse and the things which unfortunately cannot be fixed.`,
+    img: "horse-rescue.jpg",
+    date: "October 1, 2023",
+    id: "horse-rescue",
+    embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/y_2CSoiLXtw?si=qVDpB5Gyu_fMsjUA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+  },
+];
