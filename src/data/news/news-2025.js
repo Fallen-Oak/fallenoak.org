a1f47f8459c92a86216c6d61758673a68f3fb131
Brad Colangelo <bradcolangelo@gmail.com>
2025-03-19 12:59:11 -0400

    Added video and photo newspost

--- src/data/news/news-2025.js
+++ src/data/news/news-2025.js
@@ -140,4 +140,20 @@ export default [
     id: "look-whos-back",
     pony: false,
   },
+  {
+    author: "Brad Colangelo",
+    title: "Video and Photo Pages Have Been Added",
+    content: `Have you missed out on any of Fallen Oak’s convention panels or livestreams? Don’t worry, we’ve got you covered! There’s now a video page where you can catch up on everything:
+    
+    https://pony.fallenoak.org/videos
+    
+    Would you also like to see Fallen Oak’s herd of horses? Or would you like to see what was happening on Mare Day? Well, take a gander at the photo page:
+    
+    https://pony.fallenoak.org/photos
+`,
+    img: "video-and-photo-pages",
+    date: "March 19, 2025",
+    id: "video-and-photo-pages",
+    pony: true,
+  },
 ];
