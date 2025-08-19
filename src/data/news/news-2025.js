db1e3755a530a6b0c9a519da69898aff29f8f71f
Brad Colangelo <bradcolangelo@gmail.com>
2025-08-18 23:42:25 -0400

    Updated the news section

--- src/data/news/news-2025.js
+++ src/data/news/news-2025.js
@@ -168,4 +168,91 @@ export default [
     id: "a-beautiful-horse",
     pony: false,
   },
+  {
+    author: "Kim Sweeney",
+    title: "Ladybug's Adoption",
+    content: `Are you looking for a 36 inch mini? We have a beautiful girl Ladybug, who is 18. This pony is so sweet and so pretty! She comes with 3 bags of her grain, she eats either Teff or Timothy hay, and needs to be on a dry lot or pasture with minimal grass, as she foundered 2 years ago. She is current on everything, is sound and ready to be a companion for anyone at her new home! Please conctact us if interested. Adoption fee is $500.
+    
+    UPDATE: Ladybug has found her forever home! She will be staying at our 2nd location in Ocala at the 40 acre farm. The owner of the farm has a 29 year old mini that needs a friend, and she asks that Ladybug stay to be her buddy. This lady is wonderful and allows the rescue to rent her property. I of course said yes, this is a win/win for everyone. Thanks to everyone who applied for this sweet little girl. I’m so excited that I will get to see her every day for the rest of her days. Congrats Ladybug on your forever home!`,
+    img: "ladybugs-adoption.jpg",
+    date: "April 22, 2025",
+    id: "ladybugs-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Belle's Adoption",
+    content: `Our sweet Beauty, who now goes by the name of Belle, has been adopted by the sweetest little girl. I am so thankful to Kymberly Cummings for putting a refresher on this sweet girl and reaching out to her friend Katie with a hunch! This could be a perfect partnership for her daughter. I look forward to photos and stories about the memories these two will make. Congrats, Belle! You have the privilege of being loved by a child the rest of your days, what a gift.`,
+    img: "belles-adoption.jpg",
+    date: "May 19, 2025",
+    id: "belles-adoption",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Ladybug Update",
+    content: `Update on Ladybug! She’s just livin’ her best life with Skittles and Ziggy. We are so blessed to rent this 40-acre beautiful property in Ocala. The horses are living in paradise, I will be eternally grateful to Kym and Julianne.`,
+    img: "ladybug-update.jpg",
+    date: "May 24, 2025",
+    id: "ladybug-update",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Hi, Si",
+    content: `Allow me to introduce the new guy, Si! This sweet boy came to us from a wonderful home, his mama did individual rescue for years. I was privileged to haul a couple of horses Joy rehabbed to cooler climates.
+
+    Si’s owner is moving north and no longer doing rescue she needed a soft place for him to spend the rest of his days. Si is a 25-year-old TN walker. He is the kindest soul, Trinity my daughter keeps him in shape by riding him bareback around the farm, they both thoroughly enjoy it.
+
+    I’m glad I could help someone who has helped many horses throughout the years, it is so important we help each other in the rescue world when we can. Welcome to your forever home, Si!`,
+    img: "hi-si.jpg",
+    date: "May 25, 2025",
+    id: "hi-si",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "So Long, Marley!",
+    content: `On November 11, 2023 a registered purebred, five year-old chestnut Arabian was donated to the rescue to raise funds. Marley had sustained a soft tissue injury during a storm and had been cast in his stall. Upon taking this sweet boy, we knew he had months of rehab. Time passed, many vet visits and stall rest gave way to Marley being ready to go to a new home. I was sad to see this guy go as I transported him to Georgia, but knew he would do great things. Unfortunately, Marley was unable to perform the job his adoptee needed him to do, and he was returned months later. Marley is a very high energy working horse and does not do well idle he needs a lot of structure and routine. 
+
+    Almost all the rescues are out 24/7 with access to shelters. We have a total of 13 stalls, but only use those when necessary. Marley had been stalled his whole life being let out once a day. How was he ever going to adjust to being turned out 24/7? How unfair it would be for him to be alone in the barn. We tried several approaches trying to figure out what was the best for him, and then I got a message from one of our long-time followers, Sonja Roberts, that she may have a solution. Sonja is a wonderful lady who has an Arab and happened to be friends with a wonderful lady in Ocala who had a yearling Arab and was a retired trainer. The rescue agreed to pay for all of Marley’s hay, grain, dental, vetting, Ferrier, massage chiropractic, or anything he needed. The wonderful Heather Kolman gave him her expertise care and most of all her love.
+
+    Over the past year I have seen this horse totally transform. It has been a pleasure to go visit quite often and see how he has changed. I received a call from Heather in May advising me she was moving to Danville, Kentucky and asked me if she could take Marley. I never once hesitated and tearfully told her she was such a gift to both Marley and myself. She told me she knew he was with her for a reason. Marley spending the rest of his life with Heather is the best outcome he could ever have. I went yesterday to say goodbye to him, of course he tried to bite me several times because that’s just who he is. But I could tell when I looked at his face that he was thanking me. I realized yesterday was not goodbye, as I travel to Ohio several times a year. I will just have to stop off in Kentucky to see how my favorite chestnut Arab is doing.
+
+    I will forever be grateful for both Sonja and Heather. Marley was donated to the Rescue to make money, wound up being that he cost the Rescue a lot of money. There was no adoption fee. I gladly gave him to Heather, because there is no way to put a price on what that sweet boy was worth and all that she has done for him. So today as they take their 10 1/2 hour trip to their forever home. I am sad, but I am overjoyed because this is the perfect life for our sweet boy.
+
+    Congrats, Heather! Many blessings to you as you start your new adventure with your horse, who is now lovingly referred to as Red.`,
+    img: "so-long-marley.jpg",
+    date: "June 10, 2025",
+    id: "so-long-marley",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Big Red's Passing",
+    content: `Very sad day here at the rescue, as our beloved Big Red passed away. All was well at feeding time this morning and when I came back around 2:30 to drop off grain order, he was gone. Red was only 14 years old. It’s as he just lay down and went to sleep, I found him in the pasture and that is where he will rest until we meet again.
+
+    I was privileged to know and love this guy for 10 months and he was one of my daughter’s favorites, along with one of our volunteers Eddie. Big Red was a special guy. We came so far working with the Ferrier and running with his herd and loving kisses.
+
+    I expect to lose the very old horses, the sick horses, but not one that appeared to be completely healthy and was young. Sometimes I just don’t understand. Sunny’s gonna miss you big guy, give Earl a whinny for me. I wish we would’ve had more time.
+
+    Update: It appears he was struck by lightning, after further examination of his body. While he was being buried, he had all the tale tell signs. This is the first time I’ve ever experienced anything like this in my 21 years of owning horses. I’m thankful we have a probable answer, but still wishing we had more time. I pray it was immediate and he did not suffer.`,
+    img: "big-reds-passing.jpg",
+    date: "July 2, 2025",
+    id: "big-reds-passing",
+    pony: false,
+  },
+  {
+    author: "Kim Sweeney",
+    title: "Beauty up for Adoption",
+    content: `Beauty, aka Phantom Girl, is a 15-year-old off the track thoroughbred. Beauty stands 16’3, is currently barefoot, and is up-to-date on everything.
+
+    Beauty is an incredible mover, has potential for jumping is ridden both English and western, but would love to see her go to an English riding home, where she can excel and be shown. Beauty was returned by a prior adopter after 6 weeks, as she was too much horse for her 8-year-old.
+
+    Beauty is fine for a child as long as they are a confident rider. Beauty is currently located at our trainers in Ocala. We have tons of riding videos we can send you. If you would like to come see her, please reach out. Let’s find this sweet girl her forever home. Adoption fee is $1,200.`,
+    img: "beauty-up-for-adoption.jpg",
+    date: "August 2, 2025",
+    id: "beauty-up-for-adoption",
+    pony: false,
+  },
 ];
